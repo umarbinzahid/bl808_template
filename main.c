ecae8fb2ad3a7c4d79f08ddac5acde7c09bc9344
umarbinzahid <142439476+umarbinzahid@users.noreply.github.com>
2023-09-24 17:17:30 +0500

    Added changes for c906 CPU.

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
-#include "bflb_gpio.h"
+#include "bflb_mtimer.h"
 #include "board.h"
+#include "bflb_gpio.h"
 
 struct bflb_device_s *gpio;
 
 int main(void)
 {
     board_init();
-
     gpio = bflb_device_get_by_name("gpio");
-    printf("gpio output\r\n");
     bflb_gpio_init(gpio, GPIO_PIN_8, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_0);
-
     while (1) {
-        bflb_gpio_set(gpio, GPIO_PIN_8);
-        bflb_mtimer_delay_ms(1000);
 
+        bflb_gpio_set(gpio, GPIO_PIN_8);
+        printf("LED ON\r\n");
+        bflb_mtimer_delay_ms(1000);
         bflb_gpio_reset(gpio, GPIO_PIN_8);
         bflb_mtimer_delay_ms(1000);
+        printf("LED OFF\r\n");
     }
 }
